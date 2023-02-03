79d7ac4b59b5ba3fc10f72317423f55edfa6bda8
Gui Vieira <guialbuk@gmail.com>
2023-02-02 20:13:29 -0800

    Fix tests and linting

--- test/test_hello_rust.rb
+++ test/test_hello_rust.rb
@@ -4,10 +4,10 @@ require "test_helper"
 
 class TestHelloRust < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::HelloRust::VERSION
+    refute_nil(::HelloRust::VERSION)
   end
 
   def test_it_does_something_useful
-    assert false
+    assert_equal("Hello from Rust, Ferris!", HelloRust.hello("Ferris"))
   end
 end
